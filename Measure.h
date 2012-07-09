9f32c84e3563db16a1ae065d715c573122bf8c13
Nicolas Seriot <nicolas.seriot@swissquote.com>
2012-07-09 15:26:51 +0200

    works on iOS 6

--- Measure.h
+++ Measure.h
@@ -7,7 +7,34 @@
 //
 
 #import <Foundation/Foundation.h>
-#import "UIStatusBarServerThread.h"
+
+typedef struct { // UIStatusBarServerThread
+	BOOL itemIsEnabled[23];
+	BOOL timeString[64];
+	int gsmSignalStrengthRaw;
+	int gsmSignalStrengthBars;
+	BOOL serviceString[100];
+	BOOL serviceCrossfadeString[100];
+	BOOL serviceImages[3][100];
+	BOOL operatorDirectory[1024];
+	unsigned int serviceContentType;
+	int wifiSignalStrengthRaw;
+	int wifiSignalStrengthBars;
+	unsigned int dataNetworkType;
+	int batteryCapacity;
+	unsigned int batteryState;
+	BOOL notChargingString[150];
+	int bluetoothBatteryCapacity;
+	int thermalColor;
+	unsigned int thermalSunlightMode : 1;
+	unsigned int slowActivity : 1;
+	unsigned int syncActivity : 1;
+	BOOL activityDisplayId[256];
+	unsigned int bluetoothConnected : 1;
+	unsigned int displayRawGSMSignal : 1;
+	unsigned int displayRawWifiSignal : 1;
+	unsigned int locationIconType : 1;
+} StatusBarData;
 
 @class CLLocation;
 
--- Measure.m
+++ Measure.m
@@ -7,8 +7,10 @@
 //
 
 #import "Measure.h"
-#import "UIStatusBarServerThread.h"
 #import <CoreLocation/CoreLocation.h>
+
+@class UIStatusBarForegroundView;
+@class UIStatusBarDataNetworkItemView;
 
 @implementation Measure
 
@@ -18,11 +20,33 @@
 @synthesize date;
 @synthesize location;
 
++ (NSNumber *)dataNetworkTypeFromStatusBar {
+    
+    UIApplication *app = [UIApplication sharedApplication];
+    
+    UIStatusBar *statusBar = [app valueForKey:@"statusBar"];
+    
+    UIStatusBarForegroundView *foregroundView = [statusBar valueForKey:@"foregroundView"];
+    
+    NSArray *subviews = [foregroundView subviews];
+    
+    UIStatusBarDataNetworkItemView *dataNetworkItemView = nil;
+    
+    for (id subview in subviews) {
+        if([subview isKindOfClass:[NSClassFromString(@"UIStatusBarDataNetworkItemView") class]]) {
+            dataNetworkItemView = subview;
+            break;
+        }
+    }
+    
+    return [dataNetworkItemView valueForKey:@"dataNetworkType"];
+}
+
 + (NSString *)stringForNetworkType:(NSUInteger)aNetworkType {
 	if(aNetworkType == 0) return @"GSM";
 	if(aNetworkType == 1) return @"Edge";
 	if(aNetworkType == 2) return @"3G";
-	if(aNetworkType == 3) return @"Wifi";
+	if(aNetworkType == 5) return @"Wifi";
 	
 	return nil;
 }
@@ -42,9 +66,10 @@
 + (Measure *)measureWithStatusBarData:(StatusBarData *)statusBarData location:(CLLocation *)aLocation {
 	
 	NSInteger gsmSignalStrengthRaw = statusBarData->gsmSignalStrengthRaw;
-	NSInteger dataNetworkType = statusBarData->dataNetworkType;
+	//NSInteger dataNetworkType = statusBarData->dataNetworkType; // always 0
+    NSUInteger dataNetworkType = [[self dataNetworkTypeFromStatusBar] integerValue];
 	char* serviceString = (char *)statusBarData->serviceString;
-	
+	    
 	NSString *aNetworkName = [NSString stringWithCString:serviceString encoding:NSUTF8StringEncoding];
 
 	Measure *m = [[Measure alloc] init];
