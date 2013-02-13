1e5813bf0984a39e58c89d3e05f26d97e4993edc
Nicolas Seriot <nicolas.seriot@swissquote.com>
2013-02-13 13:29:59 +0100

    updated private APIs for iOS 6.1

--- Classes/UIApplication+MS.h
+++ Classes/UIApplication+MS.h
@@ -9,31 +9,31 @@
 #import <UIKit/UIKit.h>
 
 typedef struct { // from UIStatusBarServerThread
-	BOOL itemIsEnabled[23];
-	BOOL timeString[64];
-	int gsmSignalStrengthRaw;
-	int gsmSignalStrengthBars;
-	BOOL serviceString[100];
-	BOOL serviceCrossfadeString[100];
-	BOOL serviceImages[3][100];
-	BOOL operatorDirectory[1024];
-	unsigned int serviceContentType;
-	int wifiSignalStrengthRaw;
-	int wifiSignalStrengthBars;
-	unsigned int dataNetworkType;
-	int batteryCapacity;
-	unsigned int batteryState;
-	BOOL notChargingString[150];
-	int bluetoothBatteryCapacity;
-	int thermalColor;
-	unsigned int thermalSunlightMode : 1;
-	unsigned int slowActivity : 1;
-	unsigned int syncActivity : 1;
-	BOOL activityDisplayId[256];
-	unsigned int bluetoothConnected : 1;
-	unsigned int displayRawGSMSignal : 1;
-	unsigned int displayRawWifiSignal : 1;
-	unsigned int locationIconType : 1;
+    BOOL itemIsEnabled[24];
+    BOOL timeString[64];
+    int gsmSignalStrengthRaw;
+    int gsmSignalStrengthBars;
+    BOOL serviceString[100];
+    BOOL serviceCrossfadeString[100];
+    BOOL serviceImages[2][100];
+    BOOL operatorDirectory[1024];
+    unsigned int serviceContentType;
+    int wifiSignalStrengthRaw;
+    int wifiSignalStrengthBars;
+    unsigned int dataNetworkType;
+    int batteryCapacity;
+    unsigned int batteryState;
+    BOOL batteryDetailString[150];
+    int bluetoothBatteryCapacity;
+    int thermalColor;
+    unsigned int thermalSunlightMode : 1;
+    unsigned int slowActivity : 1;
+    unsigned int syncActivity : 1;
+    BOOL activityDisplayId[256];
+    unsigned int bluetoothConnected : 1;
+    unsigned int displayRawGSMSignal : 1;
+    unsigned int displayRawWifiSignal : 1;
+    unsigned int locationIconType : 1;
 } StatusBarData;
 
 @interface UIApplication (MS)
